488b670b74657695f9ad651e94743517f576e6e9
Manuel Santos Souza <89389164+manuelzzz@users.noreply.github.com>
2025-05-29 17:13:59 +0000

    feat: Adicionando as opções a serem selecionadas pelo usuário

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,69 @@
-//
-// Created by migue on 28/05/2025.
-//
+#include "../include/lse.hpp"
+
+void exibirOpcoes();
+
+int main()
+{
+    Lse lista = Lse();
+    int opcao;
+
+    std::cout << "Bem vindo ao gerenciador de tarefas!" << std::endl;
+
+    while (true)
+    {
+        exibirOpcoes();
+        std::cin >> opcao;
+
+        switch (opcao)
+        {
+        case 1:
+            lista.exibir();
+            break;
+        case 2:
+        {
+            Tarefa novaTarefa;
+            std::cout << "Digite a descricao da tarefa: ";
+            std::cin.ignore();
+
+            std::getline(std::cin, novaTarefa.descricao);
+            std::cout << "Digite a prioridade da tarefa (1-3): ";
+            int prioridade;
+            std::cin >> prioridade;
+            novaTarefa.prioridade = static_cast<Prioridade>(prioridade);
+
+            lista.inserir(novaTarefa);
+            break;
+        }
+        case 3:
+        {
+            std::cout << "Em desenvolvimento." << std::endl;
+            break;
+        }
+        case 4:
+        {
+            std::cout << "Em desenvolvimento." << std::endl;
+            break;
+        }
+        case 5:
+        {
+            std::cout << "Saindo do gerenciador de tarefas. Até logo!" << std::endl;
+            return 0;
+        }
+
+        default:
+            std::cout << "Opcao invalida. Tente novamente." << std::endl;
+        }
+    }
+
+    return 0;
+}
+
+void exibirOpcoes()
+{
+    std::cout << "O que deseja fazer?" << std::endl;
+    std::cout << "1. Visualizar suas tarefas" << std::endl;
+    std::cout << "2. Adicionar uma nova tarefa" << std::endl;
+    std::cout << "3. Remover uma tarefa" << std::endl;
+    std::cout << "4. Buscar uma tarefa" << std::endl;
+    std::cout << "5. Sair" << std::endl;
+}
