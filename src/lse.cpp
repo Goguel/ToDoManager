f19ad9752398968724673c57858b648e6fb485fb
Manuel Santos Souza <89389164+manuelzzz@users.noreply.github.com>
2025-05-29 20:16:05 +0000

    fix: Implementação da inserção de Tarefas

--- src/lse.cpp
+++ src/lse.cpp
@@ -44,7 +44,16 @@ void Lse::inserir(Tarefa novaTarefa)
     nn->value = novaTarefa;
     nn->proximo = nullptr;
 
-    _buscarUltimo(_head)->proximo = nn;
+    No<Tarefa> *ultimo = _buscarUltimo(_head);
+
+    if (ultimo == nullptr)
+    {
+        _head = nn;
+        return;
+        return;
+    }
+
+    ultimo->proximo = nn;
 }
 
 void Lse::exibir()
