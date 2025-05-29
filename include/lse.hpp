be965dad2df67f4b4de7b8768c9df2c1a322004f
Manuel Santos Souza <89389164+manuelzzz@users.noreply.github.com>
2025-05-29 17:12:54 +0000

    feat: Adicionando os arquivos de cabeçalho

---
+++ include/lse.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "no.hpp"
+#include "prioridade.hpp"
+#include "tarefa.hpp"
+
+class Lse
+{
+private:
+    No<Tarefa> *_head;
+    No<Tarefa> *_buscarUltimo(No<Tarefa> *h);
+    // void _ordenar();
+    void _exibirSeparador();
+
+public:
+    Lse();
+    Lse(Tarefa tarefa);
+
+    static int length(No<Tarefa> *h);
+
+    void inserir(Tarefa novaTarefa);
+    // void remover(int id);
+    void remover(Prioridade prioridade);
+    // void buscar(int id);
+    void exibir();
+};
---
+++ include/prioridade.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum Prioridade
+{
+    ALTA = 1,
+    MEDIA,
+    BAIXA,
+};
