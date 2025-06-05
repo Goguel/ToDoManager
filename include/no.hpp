7dc0a9fd75563b3d1d9c9b10c6d1b8c4b2a2b032
Luan Sampaio <luansampaio0294@gmail.com>
2025-06-05 15:36:06 -0300

    doc: no, prioridade e tarefa

--- include/no.hpp
+++ include/no.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+/**
+ * @brief Representa um nó de uma lista encadeada simples.
+ * @tparam T Tipo do valor armazenado no nó.
+ */
 template <class T>
 struct No
 {
-    T value;
-    No *proximo;
+    T value;        ///< Valor armazenado no nó.
+    No *proximo;    ///< Ponteiro para o próximo nó.
 };
--- include/tarefa.hpp
+++ include/tarefa.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <iostream>
-
+/**
+ * @brief Estrutura que representa uma tarefa.
+ * @details Essa struct será usada como dado do nó da lista encadeada.
+ */
 struct Tarefa
 {
-    int id;
-    std::string descricao;
-    int prioridade;
+    int id;                 ///< O id representa o índice de cada tarefa.
+    std::string descricao;  ///< Variável para armazenar a descrição inserida.
+    int prioridade;         ///< Prioridades atribuídas: alta, média e baixa.
 };
